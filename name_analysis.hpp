dea96ea7855cc10cb0fe1bc4e1379f6ac7b092f7
SaltyFishCCC <34460474+ZZHCzzhc@users.noreply.github.com>
2021-10-07 02:13:45 -0500

    new
    new

---
+++ name_analysis.hpp
@@ -0,0 +1,31 @@
+#ifndef CSHANTYC_NAME_ANALYSIS
+#define CSHANTYC_NAME_ANALYSIS
+
+#include "ast.hpp"
+#include "symbol_table.hpp"
+
+namespace cshanty{
+
+class NameAnalysis{
+public:
+	static NameAnalysis * build(ProgramNode * astIn){
+		NameAnalysis * nameAnalysis = new NameAnalysis;
+		SymbolTable * symTab = new SymbolTable();
+		bool res = astIn->nameAnalysis(symTab);
+		delete symTab;
+		if (!res){ return nullptr; }
+
+		nameAnalysis->ast = astIn;
+		return nameAnalysis;
+	}
+
+	ProgramNode * ast;
+
+private:
+	NameAnalysis(){
+	}
+};
+
+}
+
+#endif
